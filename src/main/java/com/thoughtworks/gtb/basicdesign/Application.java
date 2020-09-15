7ad081bb4baf5107c8c55fcace6688b020a95d05
HXgrowns <804156017@qq.com>
2020-09-15 11:19:13 +0800

    [huxiao]: init project

---
+++ src/main/java/com/thoughtworks/gtb/basicdesign/Application.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.gtb.basicdesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
